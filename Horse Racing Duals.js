f6cfd4e33b858ed7b8764e73d0204711b2b6fd91
Asma <asma.hussaini@zartis.com>
2022-10-13 14:42:51 +0100

    use sort method to sort the list to improve time operation

--- Horse Racing Duals.js
+++ Horse Racing Duals.js
@@ -5,3 +5,8 @@ for (var i = 0; i < N; i++) {
     sortedStrengthsArray.push(parseInt(pi));
 }
 
+var minDiff = 10000000;
+sortedStrengthsArray = sortedStrengthsArray.sort(function(a, b) {
+    return (b - a);
+});
+
