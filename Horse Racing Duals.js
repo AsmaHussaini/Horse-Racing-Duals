519e3b4c8bcf31793e3feab9260190c221c1fdd0
Asma <asma.hussaini@zartis.com>
2022-10-13 14:41:06 +0100

    add for loop to store all values in the 'sortedStrenghtArray' list

--- Horse Racing Duals.js
+++ Horse Racing Duals.js
@@ -1,5 +1,7 @@
 var N = parseInt(readline());
 var sortedStrengthsArray = [];
-
-
+for (var i = 0; i < N; i++) {
+    var pi = parseInt(readline());
+    sortedStrengthsArray.push(parseInt(pi));
+}
 
